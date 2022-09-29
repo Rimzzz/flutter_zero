f7932b518f88a1fe5a330bdffa7f164f4ef70489
Taufiq <htaufiq570@gmail.com>
2022-09-30 06:06:49 +0700

    List

--- lib/list.dart
+++ lib/list.dart
@@ -1,38 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/listData.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyList());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyList extends StatelessWidget {
+  const MyList({super.key});
+  // final List<int> colorCode = <int>[700, 600, 500, 400, 300, 200, 100];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: SafeArea(
-        child: Container(
-          color: Colors.blue,
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            children: [
-              Container(
-                color: Colors.amber[700],
-                height: 300,
-              ),
-              Container(
-                color: Colors.amber[500],
-                height: 300,
-              ),
-              Container(
-                color: Colors.amber[100],
-                height: 300,
-              )
-            ],
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            // child: ListView(
+            //   padding: const EdgeInsets.all(16),
+            //   scrollDirection: Axis.horizontal,
+            //   reverse: true,
+            //   physics: const NeverScrollableScrollPhysics(),
+            //   shrinkWrap: true,
+            //   children: [
+            //     Container(
+            //       color: Colors.amber[700],
+            //       height: 300,
+            //       width: 300,
+            //     ),
+            //     Container(
+            //       color: Colors.amber[500],
+            //       height: 300,
+            //       width: 300,
+            //     ),
+            //     Container(
+            //       color: Colors.amber[300],
+            //       height: 300,
+            //       width: 300,
+            //     )
+            //   ],
+            // ),
+            // -- LIST BUILDER --
+            // child: ListView.builder(
+            //   padding: const EdgeInsets.all(10),
+            //   itemCount: colorCode.length,
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Container(
+            //       height: 100,
+            //       color: Colors.amber[colorCode[index]],
+            //     );
+            //   },
+            // ),
+            // -- SEPARATED --
+            // child: ListView.separated(
+            //   padding: const EdgeInsets.all(10),
+            //   itemCount: colorCode.length,
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Container(
+            //       height: 100,
+            //       color: Colors.amber[colorCode[index]],
+            //     );
+            //   },
+            //   separatorBuilder: (BuildContext context, int index) {
+            //     return const Divider();
+            //   },
+            // ),
+            // -- LIST TILE --
+            child: ListView.separated(
+              padding: const EdgeInsets.all(10),
+              itemCount: dataFromApi.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: Image.network(dataFromApi[index]['url']!),
+                  title: Text(dataFromApi[index]['text']!),
+                  subtitle: Text(dataFromApi[index]['subtitle']!),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
---
+++ lib/listData.dart
@@ -0,0 +1,32 @@
+var dataFromApi = <Map<String, String>>[
+  {
+    'url':
+        'https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',
+    'text': 'List Satu',
+    'subtitle': 'Coba coba list'
+  },
+  {
+    'url':
+        'https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',
+    'text': 'List Dua',
+    'subtitle': 'Coba coba list'
+  },
+  {
+    'url':
+        'https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',
+    'text': 'List Tiga',
+    'subtitle': 'Coba coba list'
+  },
+  {
+    'url':
+        'https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',
+    'text': 'List Empat',
+    'subtitle': 'Coba coba list'
+  },
+  {
+    'url':
+        'https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',
+    'text': 'List Lima',
+    'subtitle': 'Coba coba list'
+  },
+];
